48aa47820793c7e773fd071fb46ddbfa7ac46028
SamuelRobart <samuelolivarobart2000@gmail.com>
2023-07-24 13:57:24 +0530

    first-change

---
+++ Ample.java
@@ -0,0 +1,42 @@
+class Ample
+{
+    public static void main(String arh[])
+    {
+        int a=103;
+    boolean bool=true;
+        
+        
+        if(a==2)
+        {
+            System.out.println("The given numnber prime:"+a);
+            bool=false;
+        }
+        else
+        {
+
+            for(int i=3;i<a;i+=2)
+            {
+                if(a%2==0)
+                {
+                    System.out.println("The given numnber not prime:"+a);
+                    bool=false;
+                    break;
+                }
+
+                if(a%i==0)
+                {
+                    System.out.println("The given numnber not prime:"+a);
+                    bool=false;
+                    break;
+                }
+            }
+        }
+
+        if(bool==true)
+        {
+        
+            System.out.println("The given numnber prime:"+a);
+            
+        }
+    }
+}
